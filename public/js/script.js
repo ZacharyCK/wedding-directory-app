dab8ec274aea882861e0f07295f1392620d39db4
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-08-26 15:57:20 -0400

    continued to work on the functionality of the add guest form

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,68 @@
+// submiting the form to add a new guest
+const newGuestForm = document.getElementById("new-guest");
+newGuestForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const numberOfGuests = document.getElementById("guest-range").value;
+  const guestEmail = document.getElementById("guest-email").value;
+  const guestPhone = document.getElementById("guest-phone").value;
+  const guestAddress = document.getElementById("guest-address").value;
+  const guestAllergies = document.getElementById("guest-allergies").value;
+  const guestBlockOutDate1 = document.getElementById(
+    "guest-blockout-date1"
+  ).value;
+  const weddingPartyGuest = weddingPartyCheckbox.checked ? true : false;
+  let guestRole;
+  let roleClass;
+  let pjShirtSize;
+  let pjShortSize;
+  let favoriteColor;
+  let favoriteSnack;
+  let favoriteCandy;
+  let favoriteAlcohol;
+  let favoriteNonAlcohol;
+  if (document.getElementById("weddingPartyData")) {
+    guestRole = document.getElementById("guest-role");
+    roleClass = document.getElementById("role-class");
+    pjShirtSize = document.getElementById("pj-shirt-size");
+    pjShortSize = document.getElementById("pj-short-size");
+    favoriteColor = document.getElementById("fav-color");
+    favoriteSnack = document.getElementById("fav-snack");
+    favoriteCandy = document.getElementById("fav-candy");
+    favoriteAlcohol = document.getElementById("fav-alc");
+    favoriteNonAlcohol = document.getElementById("fav-non-alc");
+  }
+  const newGuest = {
+    firstName: firstName,
+    lastName: lastName,
+    email: guestEmail,
+    phone: guestPhone,
+    address: guestAddress,
+    numOfGuests: numberOfGuests,
+    allergies: [guestAllergies],
+    blockOutDates: [guestBlockOutDate1],
+    isWeddingParty: weddingPartyGuest,
+    role: guestRole ? guestRole : "N/A",
+    roleClass: roleClass ? roleClass : "N/A",
+    pajamaSizes: {
+      shirt: pjShirtSize ? pjShirtSize : "N/A",
+      shorts: pjShortSize ? pjShortSize : "N/A",
+    },
+    favorites: {
+      color: favoriteColor ? favoriteColor : "N/A",
+      snack: favoriteSnack ? favoriteSnack : "N/A",
+      candy: favoriteCandy ? favoriteCandy : "N/A",
+      alcohol: favoriteAlcohol ? favoriteAlcohol : "N/A",
+      nonAlcohol: favoriteNonAlcohol ? favoriteNonAlcohol : "N/A",
+    },
+  };
+  console.log(newGuest);
+});
+
 // Form range for number of guests
 const guestRangeLabelValue = document.getElementById("rangeValue");
-const guestRangeValue = document.getElementById("guestsRange");
+const guestRangeValue = document.getElementById("guest-range");
 
 // Update the label with the current value
 guestRangeValue.addEventListener("input", function () {
@@ -8,7 +70,7 @@ guestRangeValue.addEventListener("input", function () {
 });
 
 // Checkbox logic when wedding party checkbox is checked
-const weddingPartyCheckbox = document.getElementById("weddingParty");
+const weddingPartyCheckbox = document.getElementById("wedding-party");
 const formSubmitButton = document.getElementById("submitGuest");
 
 //checkbox event listener
@@ -19,9 +81,9 @@ weddingPartyCheckbox.addEventListener("change", function () {
       `
         <div id="weddingPartyData">
           <div class="mb-3">
-            <label for="guestRole" class="form-label"
+            <label for="guest-role" class="form-label"
               >Guest Role</label>
-            <select id="guestRole" class="form-select" aria-label="Default select example">
+            <select id="guest-role" class="form-select" aria-label="Default select example">
               <option selected>Choose Role</option>
               <option value="best man">Best Man</option>
               <option value="maid of honor">Maid of Honor</option>
@@ -33,14 +95,70 @@ weddingPartyCheckbox.addEventListener("change", function () {
             </select>
           </div>
           <div class="mb-3">
-            <label for="guestRole" class="form-label"
+            <label for="role-class" class="form-label"
               >Role Class</label>
-            <select id="guestRole" class="form-select" aria-label="Default select example">
+            <select id="role-class" class="form-select" aria-label="Default select example">
               <option selected>Choose Role Class</option>
               <option value="groomsmen">Groomsmen</option>
               <option value="bridesmaids">Bridsmaids</option>
               <option value="parents">Parents</option>
             </select>
+          </div>
+          <div class="mb-3">
+            <label for="pj-shirt-size" class="form-label"
+              >Pajama Shirt Size</label>
+            <select id="pj-shirt-size" class="form-select" aria-label="Default select example">
+              <option selected>Choose Shirt Size</option>
+              <option value="extra-small">XS</option>
+              <option value="small">S</option>
+              <option value="medium">M</option>
+              <option value="large">L</option>
+              <option value="extra-large">XL</option>
+              <option value="extra-extra-large">XXL</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="pj-short-size" class="form-label"
+              >Pajama Short Size</label>
+            <select id="pj-short-size" class="form-select" aria-label="Default select example">
+              <option selected>Choose Short Size</option>
+              <option value="extra-small">XS</option>
+              <option value="small">S</option>
+              <option value="medium">M</option>
+              <option value="large">L</option>
+              <option value="extra-large">XL</option>
+              <option value="extra-extra-large">XXL</option>
+            </select>
+          </div>
+          <div class="mb-3">
+                <label for="fav-color" class="form-label"
+                  >Favorite Color</label
+                >
+                <input type="text" class="form-control" id="fav-color" />
+          </div>
+          <div class="mb-3">
+                <label for="fav-snack" class="form-label"
+                  >Favorite Snack</label
+                >
+                <input type="text" class="form-control" id="fav-snack" />
+          </div>
+          <div class="mb-3">
+                <label for="fav-candy" class="form-label"
+                  >Favorite Candy</label
+                >
+                <input type="text" class="form-control" id="fav-candy" />
+          </div>
+          <div class="mb-3">
+                <label for="fav-alc" class="form-label"
+                  >Favorite Alcoholic Drink</label
+                >
+                <input type="text" class="form-control" id="fav-alc" />
+          </div>
+          <div class="mb-3">
+                <label for="fav-non-alc" class="form-label"
+                  >Favorite Non-Alcoholic Drink</label
+                >
+                <input type="text" class="form-control" id="fav-non-alc" />
           </div>
         </div>
       `
